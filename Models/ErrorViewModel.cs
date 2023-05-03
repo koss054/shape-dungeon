bc8ff067a87c04dc7925a454d5dbe7b9d5d49c63
koss054 <konstantin054@abv.bg>
2023-05-03 11:00:16 +0300

    Create initial project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShapeDungeon.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
