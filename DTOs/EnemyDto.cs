83889961c57b250dc985cf219ae9a6e344b28b85
koss054 <konstantin054@abv.bg>
2023-05-05 19:06:48 +0300

    Add 3 new DTOs

---
+++ DTOs/EnemyDto.cs
@@ -0,0 +1,14 @@
+﻿namespace ShapeDungeon.DTOs
+{
+    public class EnemyDto
+    {
+        public string Name { get; set; }
+            = null!;
+
+        public int Strength { get; set; }
+        public int Vigor { get; set; }
+        public int Agility { get; set; }
+        public int Level { get; set; }
+        public int DroppedExp { get; set; }
+    }
+}
---
+++ DTOs/GameDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ShapeDungeon.DTOs
+{
+    public class GameDto
+    {
+        public PlayerDto Player { get; set; }
+            = null!;
+
+        public RoomDto Room { get; set; }
+            = null!;
+
+        public EnemyDto? Enemy { get; set; }
+    }
+}
