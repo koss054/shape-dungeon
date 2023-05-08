2ca7ac274b51566fe777ea28b29ae91ddc3918b5
koss054 <konstantin054@abv.bg>
2023-05-08 19:14:43 +0300

    Create initial Create room controller get/post

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -1,9 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShapeDungeon.DTOs;
+using ShapeDungeon.Interfaces.Services;
 
 namespace ShapeDungeon.Controllers
 {
     public class RoomController : Controller
     {
-        
+        private readonly IRoomService _roomService;
+
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(RoomDto room)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Invalid room!";
+                return View(room);
+            }
+
+            await _roomService.CreateRoomAsync(room);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
