b74ec73e900f52dd93f0e7efe3807af58e8ed324
koss054 <konstantin054@abv.bg>
2023-05-08 16:38:33 +0300

    Create Room controller and nav DTO

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShapeDungeon.Controllers
+{
+    public class RoomController : Controller
+    {
+        
+    }
+}
---
+++ DTOs/RoomNavDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ShapeDungeon.DTOs
+{
+    public class RoomNavDto
+    {
+        public Guid Id { get; init; }
+        public Guid? LeftRoomId { get; init; }
+        public Guid? RightRoomId { get; init; }
+        public Guid? TopRoomId { get; init; }
+        public Guid? DownRoomId { get; init; }
+    }
+}
