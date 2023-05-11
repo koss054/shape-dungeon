3e685ca3b5ad3c261edeb569ca21ee834266fe9c
koss054 <konstantin054@abv.bg>
2023-05-11 15:47:26 +0300

    Update migrations with IsActiveForEditRoomProp

---
+++ Migrations/20230511124605_IsActiveForEditRoomProp.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShapeDungeon.Migrations
+{
+    public partial class IsActiveForEditRoomProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActiveForEdit",
+                table: "Rooms",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActiveForEdit",
+                table: "Rooms");
+        }
+    }
+}
