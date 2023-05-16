435d7f91c4d85e901740306cc613ebd6368002aa
koss054 <konstantin054@abv.bg>
2023-05-16 12:36:29 +0300

    Create initial RoomCreateService

---
+++ Interfaces/Services/Rooms/IRoomCreateService.cs
@@ -0,0 +1,18 @@
+﻿using ShapeDungeon.DTOs.Room;
+
+namespace ShapeDungeon.Interfaces.Services.Rooms
+{
+    public interface IRoomCreateService
+    {
+        // Methods used to initialize room creation page.
+        // Populate the RoomCreateDto used in the creation page.
+        RoomCreateDto InitializeLeftRoom(Guid rightRoomId);
+        RoomCreateDto InitializeRightRoom(Guid leftRoomId);
+        RoomCreateDto InitializeUpRoom(Guid bottomRoomId);
+        RoomCreateDto InitializeBottomRoom(Guid upRoomId);
+
+        // Method used when user is on room creation page.
+        // Happens when a blue "Create" button is pressed.
+        Task<bool> CreateRoomAsync(RoomCreateDto roomDto);
+    }
+}
---
+++ Services/Rooms/RoomCreateService.cs
@@ -0,0 +1,55 @@
+﻿using ShapeDungeon.DTOs.Room;
+using ShapeDungeon.Entities;
+using ShapeDungeon.Interfaces.Repositories;
+using ShapeDungeon.Interfaces.Services.Rooms;
+
+namespace ShapeDungeon.Services.Rooms
+{
+    public class RoomCreateService : IRoomCreateService
+    {
+        private readonly IDbContext _context;
+
+        public RoomCreateService(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateRoomAsync(RoomCreateDto roomDto)
+        {
+            Room room = new()
+            {
+                IsActive = false,
+                IsActiveForEdit = true,
+                CanGoLeft = roomDto.CanGoLeft,
+                CanGoRight = roomDto.CanGoRight,
+                CanGoUp = roomDto.CanGoUp,
+                CanGoDown = roomDto.CanGoDown,
+                IsStartRoom = roomDto.IsStartRoom,
+                IsSafeRoom = roomDto.IsSafeRoom,
+                IsEnemyRoom = roomDto.IsEnemyRoom,
+                IsEndRoom = roomDto.IsEndRoom,
+                Enemy = null,
+                LeftRoomId = roomDto.LeftRoomId,
+                RightRoomId = roomDto.RightRoomId,
+                TopRoomId = roomDto.TopRoomId,
+                DownRoomId = roomDto.DownRoomId,
+            };
+
+            await _context.Rooms.AddAsync(room);
+            var result = await _context.SaveChangesAsync();
+            return result > 0;
+        }
+
+        public RoomCreateDto InitializeLeftRoom(Guid rightRoomId)
+            => new() { CanGoRight = true, RightRoomId = rightRoomId };
+
+        public RoomCreateDto InitializeRightRoom(Guid leftRoomId)
+            => new() { CanGoLeft = true, LeftRoomId = leftRoomId };
+
+        public RoomCreateDto InitializeUpRoom(Guid bottomRoomId)
+            => new() { CanGoDown = true, DownRoomId = bottomRoomId };
+
+        public RoomCreateDto InitializeBottomRoom(Guid upRoomId)
+            => new() { CanGoUp = true, TopRoomId = upRoomId };
+    }
+}
