e59642194350fbd738c593beea98d3fa8928f69c
koss054 <konstantin054@abv.bg>
2023-05-19 15:39:59 +0300

    Update RoomController.cs
    Update renamed RoomService.
    Guarantee roomDetails in SetDtoNeighborAsync won't be null.
    Add new Go action for changing IsActiveForEdit when creating rooms.

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -7,20 +7,20 @@ namespace ShapeDungeon.Controllers
 {
     public class RoomController : Controller
     {
-        private readonly IRoomService _roomService;
         private readonly IGetRoomService _getRoomService;
         private readonly IRoomCreateService _roomCreateService;
+        private readonly IRoomActiveForEditService _roomActiveForEditService;
         private readonly ICheckRoomNeighborsService _checkRoomNeighborsService;
 
         public RoomController(
-            IRoomService roomService,
             IGetRoomService getRoomService,
             IRoomCreateService roomCreateService, 
+            IRoomActiveForEditService roomActiveForEditService,
             ICheckRoomNeighborsService checkRoomNeighborsService)
         {
-            _roomService = roomService;
             _getRoomService = getRoomService;
             _roomCreateService = roomCreateService;
+            _roomActiveForEditService = roomActiveForEditService;
             _checkRoomNeighborsService = checkRoomNeighborsService;
         }
 
@@ -28,7 +28,7 @@ namespace ShapeDungeon.Controllers
         public async Task<IActionResult> Create()
         {
             var roomDetails = await _getRoomService.GetActiveForEditAsync();
-            var roomNav = await _checkRoomNeighborsService.SetDtoNeighborsAsync(roomDetails.CoordX, roomDetails.CoordY);
+            var roomNav = await _checkRoomNeighborsService.SetDtoNeighborsAsync(roomDetails!.CoordX, roomDetails!.CoordY);
             var room = new RoomCreateDto() { Details = roomDetails, Nav = roomNav };
             return View(room);
         }
@@ -37,7 +37,7 @@ namespace ShapeDungeon.Controllers
         public async Task<IActionResult> Create(RoomDetailsDto roomDto)
         {
             var newRoomId = await _roomCreateService.CreateAsync(roomDto);
-            await _roomService.ApplyActiveForEditAsync(newRoomId);
+            await _roomActiveForEditService.ApplyActiveForEditAsync(newRoomId);
             return RedirectToAction("Create");
         }
 
@@ -47,6 +47,13 @@ namespace ShapeDungeon.Controllers
             var roomDetails = await _roomCreateService.InitializeRoomAsync(direction);
             var room = new RoomCreateDto() { Details = roomDetails };
             return View(room);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Go(int coordX, int coordY)
+        {
+            await _roomActiveForEditService.MoveActiveForEditAsync(coordX, coordY);
+            return RedirectToAction("Create");
         }
     }
 }
