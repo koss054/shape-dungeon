f5b005cc9222f84e426ff2ab68b207bc177044a6
koss054 <konstantin054@abv.bg>
2023-05-23 11:12:24 +0300

    Move services from Program.cs to static class

---
+++ Helpers/AppServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using ShapeDungeon.Data;
+using ShapeDungeon.Interfaces.Repositories;
+using ShapeDungeon.Interfaces.Services;
+using ShapeDungeon.Interfaces.Services.Rooms;
+using ShapeDungeon.Services;
+using ShapeDungeon.Services.Rooms;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class AppServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Db
+            services.AddScoped<IDbContext, AppDbContext>();
+
+            // Player
+            services.AddScoped<IPlayerService, PlayerService>();
+
+            // Room
+            services.AddScoped<IGetRoomService, GetRoomService>();
+            services.AddScoped<IRoomCreateService, RoomCreateService>();
+            services.AddScoped<IRoomTravelService, RoomTravelService>();
+            services.AddScoped<IRoomActiveForEditService, RoomActiveForEditService>();
+            services.AddScoped<ICheckRoomNeighborsService, CheckRoomNeighborsService>();
+
+            return services;
+        }
+    }
+}
