6e33f1d0d102267f67f3b3ec2cfbcdacf01a15f4
koss054 <konstantin054@abv.bg>
2023-06-01 20:05:47 +0300

    Create RoomRepository

---
+++ Repos/IRoomRepository.cs
@@ -0,0 +1,28 @@
+﻿using ShapeDungeon.Interfaces.Entity;
+
+namespace ShapeDungeon.Repos
+{
+    public interface IRoomRepository
+    {
+        /// <summary>
+        /// Not possible for the room to be null.
+        /// Start room will always exist, and it'll have this property set to true.
+        /// </summary>
+        /// <returns>The room in which the player currently is and can move from.</returns>
+        Task<IRoom> GetActiveForMove();
+
+        /// <summary>
+        /// Not possible for the room to be null.
+        /// Start room will always exist, and it'll have this property set to true.
+        /// </summary>
+        /// <returns>The room in which the player currently is or is scouting from.</returns>
+        Task<IRoom> GetActiveForScout();
+
+        /// <summary>
+        /// Not possible for the room to be null.
+        /// Start room will always exist, and it'll have this property set to true.
+        /// </summary>
+        /// <returns>The room that is currently active in edit mode.</returns>
+        Task<IRoom> GetActiveForEdit();
+    }
+}
---
+++ Repos/RoomRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShapeDungeon.Interfaces.Entity;
+using ShapeDungeon.Interfaces.Repositories;
+
+namespace ShapeDungeon.Repos
+{
+    public class RoomRepository : RepositoryBase<IRoom>, IRoomRepository
+    {
+        public RoomRepository(IDbContext context) : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Not possible for the room to be null.
+        /// Start room will always exist, and it'll have this property set to true.
+        /// </summary>
+        /// <returns>The room in which the player currently is and can move from.</returns>
+        public async Task<IRoom> GetActiveForMove()
+            => await this.Context.Rooms.SingleAsync(x => x.IsActiveForMove);
+
+        /// <summary>
+        /// Not possible for the room to be null.
+        /// Start room will always exist, and it'll have this property set to true.
+        /// </summary>
+        /// <returns>The room in which the player currently is or is scouting from.</returns>
+        public async Task<IRoom> GetActiveForScout()
+            => await this.Context.Rooms.SingleAsync(x => x.IsActiveForScout);
+
+        /// <summary>
+        /// Not possible for the room to be null.
+        /// Start room will always exist, and it'll have this property set to true.
+        /// </summary>
+        /// <returns>The room that is currently active in edit mode.</returns>
+        public async Task<IRoom> GetActiveForEdit()
+            => await this.Context.Rooms.SingleAsync(x => x.IsActiveForEdit);
+    }
+}
