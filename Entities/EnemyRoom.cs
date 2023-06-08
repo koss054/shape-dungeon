abcdd6ca98aee4bdcac3e42a08b7a26370a9b807
koss054 <konstantin054@abv.bg>
2023-06-08 14:14:01 +0300

    Add EnemyRoom entity

---
+++ Entities/EnemyRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShapeDungeon.Entities
+{
+    public class EnemyRoom
+    {
+        [ForeignKey(nameof(Enemy))]
+        public Guid EnemyId { get; set; }
+
+        public Enemy Enemy { get; set; }
+            = null!;
+
+        [ForeignKey(nameof(Room))]
+        public Guid RoomId { get; set; }
+
+        public Room Room { get; set; }
+            = null!;
+    }
+}
