b6569863506f1e3f249c90c029971dd85a17644b
koss054 <konstantin054@abv.bg>
2023-06-14 11:54:02 +0300

    Create RoomTravelServiceTests.cs

---
+++ ShapeDungeon.Tests/ServiceTests/Rooms/RoomTravelServiceTests.cs
@@ -0,0 +1,163 @@
+﻿#nullable disable
+using Moq;
+using NUnit.Framework;
+using ShapeDungeon.Data;
+using ShapeDungeon.Entities;
+using ShapeDungeon.Helpers.Enums;
+using ShapeDungeon.Interfaces.Services.Rooms;
+using ShapeDungeon.Repos;
+using ShapeDungeon.Services.Rooms;
+using System;
+using System.Threading.Tasks;
+
+namespace ShapeDungeon.Tests.ServiceTests.Rooms
+{
+    internal class RoomTravelServiceTests
+    {
+        private Mock<IRoomRepository> _repoMock;
+        private Mock<IUnitOfWork> _unitOfWorkMock;
+        private IRoomTravelService _service;
+
+        [SetUp]
+        public void Test_Initialize()
+        {
+            _repoMock = new Mock<IRoomRepository>();
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _service = new RoomTravelService(_repoMock.Object, _unitOfWorkMock.Object);
+        }
+
+        [Test]
+        public void RoomTravel_WithInvalidAction_ThrowsOutOfRangeException()
+        {
+            // Arrange
+            var direction = RoomDirection.Left;
+            var action = 54;
+            var oldCoordX = 0;
+            var oldCoordY = 0;
+            var newCoordX = oldCoordX - 1; // Moving to the left.
+            var newCoordY = oldCoordY;
+            var oldRoom = new Room { IsActiveForMove = true, CoordX = oldCoordX, CoordY = oldCoordY };
+            var newRoom = new Room { IsActiveForMove = false, CoordX = newCoordX, CoordY = newCoordY };
+
+            _repoMock
+                .Setup(x => x.GetActiveForMove())
+                .ReturnsAsync(oldRoom);
+
+            _repoMock
+                .Setup(x => x.GetByCoords(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(newRoom);
+
+            // Act
+            var travelTask =  _service.RoomTravelAsync(direction, (RoomTravelAction)action);
+
+            // Assert
+            Assert.That(async () => await travelTask,
+                                    Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void RoomTravel_WithInvalidDirection_ThrowsOutOfRangeException()
+        {
+            // Arrange
+            var direction = 54;
+            var action = RoomTravelAction.Move;
+            var oldCoordX = 0;
+            var oldCoordY = 0;
+            var oldRoom = new Room { IsActiveForMove = true, CoordX = oldCoordX, CoordY = oldCoordY };
+
+            _repoMock
+                .Setup(x => x.GetActiveForMove())
+                .ReturnsAsync(oldRoom);
+
+            // Act
+            var travelTask = _service.RoomTravelAsync((RoomDirection)direction, action);
+
+            // Assert
+            Assert.That(async () => await travelTask,
+                                    Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void RoomTravel_WithMissingOldRoom_ThrowsNullException()
+        {
+            // Arrange
+            var direction = 54;
+            var action = RoomTravelAction.Move;
+
+            // Act
+            var travelTask = _service.RoomTravelAsync((RoomDirection)direction, action);
+
+            // Assert
+            Assert.That(async () => await travelTask,
+                                    Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public async Task ResetScout_WithValidRooms_TogglesActiveForScoutBetweenRooms()
+        {
+            // Arrange
+            var scoutRoom = new Room { IsActiveForScout = true, IsActiveForMove = false };
+            var moveRoom = new Room { IsActiveForScout = false, IsActiveForMove = true };
+
+            _repoMock
+                .Setup(x => x.GetActiveForScout())
+                .ReturnsAsync(scoutRoom);
+
+            _repoMock
+                .Setup(x => x.GetActiveForMove())
+                .ReturnsAsync(moveRoom);
+
+            // Act
+            var isSuccessful = await _service.IsScoutResetAsync();
+
+            // Assert
+            Assert.IsTrue(isSuccessful);
+        }
+
+        [Test]
+        public async Task ResetScout_WithInvalidScoutRoom_SkipsActiveForScoutToggleBetweenRooms()
+        {
+            // Arrange
+            var moveRoom = new Room { IsActiveForScout = false, IsActiveForMove = true };
+
+            _repoMock
+                .Setup(x => x.GetActiveForMove())
+                .ReturnsAsync(moveRoom);
+
+            // Act
+            var isSuccessful = await _service.IsScoutResetAsync();
+
+            // Assert
+            Assert.IsFalse(isSuccessful);
+        }
+
+        [Test]
+        public async Task ResetScout_WithInvalidMoveRoom_SkipsActiveForScoutToggleBetweenRooms()
+        {
+            // Arrange
+            var scoutRoom = new Room { IsActiveForScout = true, IsActiveForMove = false };
+
+            _repoMock
+                .Setup(x => x.GetActiveForScout())
+                .ReturnsAsync(scoutRoom);
+
+            // Act
+            var isSuccessful = await _service.IsScoutResetAsync();
+
+            // Assert
+            Assert.IsFalse(isSuccessful);
+        }
+
+        [Test]
+        public async Task ResetScout_WithInvalidRooms_SkipsActiveForScoutToggleBetweenRooms()
+        {
+            // Arrange
+
+            // Act
+            var isSuccessful = await _service.IsScoutResetAsync();
+
+            // Assert
+            Assert.IsFalse(isSuccessful);
+        }
+    }
+}
