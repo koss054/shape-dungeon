a2f6e36302eed79c1006ad1befe2ae016272e764
koss054 <konstantin054@abv.bg>
2023-06-21 11:00:27 +0300

    Create RoomEnemyService

---
+++ ShapeDungeon/Services/Rooms/RoomEnemyService.cs
@@ -0,0 +1,24 @@
+﻿using ShapeDungeon.DTOs.Enemies;
+using ShapeDungeon.Interfaces.Services.EnemiesRooms;
+using ShapeDungeon.Interfaces.Services.Rooms;
+using ShapeDungeon.Repos;
+
+namespace ShapeDungeon.Services.Rooms
+{
+    public class RoomEnemyService : IRoomEnemyService
+    {
+        private readonly IEnemiesRoomsRepository _enemiesRoomsRepository;
+
+        public RoomEnemyService(IEnemiesRoomsRepository enemiesRoomsRepository)
+        {
+            _enemiesRoomsRepository = enemiesRoomsRepository;
+        }
+
+        public async Task<EnemyDto> GetEnemy(Guid roomId)
+        {
+            var enemy = await _enemiesRoomsRepository.GetEnemyByRoomId(roomId);
+            EnemyDto enemyDto = enemy;
+            return enemyDto;
+        }
+    }
+}
