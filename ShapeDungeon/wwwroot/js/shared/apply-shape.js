13537b1042376eaaa13adc59ba9baf139b8851b3
koss054 <konstantin054@abv.bg>
2023-06-22 14:11:49 +0300

    Create apply-shape.js
    Contains a function that visualized a shape by getting the shape element it needs to update, the shape type, and the color.

---
+++ ShapeDungeon/wwwroot/js/shared/apply-shape.js
@@ -0,0 +1,28 @@
+﻿const placeholderColor = "C0L0R";
+
+const type = {
+    square: `height:10rem;width:10rem;background-color:${placeholderColor}`,
+    triangle: `width:0;height:0;border-top: 5rem solid transparent;border-left: 10rem solid ${placeholderColor};border-bottom: 5rem solid transparent`,
+    circle: `height:10rem;width:10rem;background-color:${placeholderColor};border-radius:50%`,
+    missing: `height:10rem;width:10rem;background-color:magenta;color:black;text-align:center`
+}
+
+export function updateShape(shapeElement, shapeType, shapeColor) {
+    let shapeStyle = type[shapeType];
+
+    if (shapeElement === undefined)
+        window.alert("Check the code, g, incorrect shape element passed to function.");
+
+    if (shapeStyle === undefined) {
+        shapeStyle = type["missing"];
+        shapeElement.innerText = "Unrecognized shape!";
+    } else {
+        shapeElement.innerText = "";
+    }
+
+    if (shapeColor === undefined)
+        shapeColor = "magenta"
+
+    shapeStyle = shapeStyle.replace(placeholderColor, shapeColor);
+    shapeElement.setAttribute("style", shapeStyle);
+}
