fb194faca7122c35c1056ced30ca1747efe4029b
koss054 <konstantin054@abv.bg>
2023-06-30 16:51:23 +0300

    Create CombatDto.cs

---
+++ ShapeDungeon/DTOs/CombatDto.cs
@@ -0,0 +1,16 @@
+﻿using ShapeDungeon.DTOs.Enemies;
+using ShapeDungeon.DTOs.Players;
+
+namespace ShapeDungeon.DTOs
+{
+    public class CombatDto
+    {
+        public PlayerDto Player { get; init; }
+            = null!;
+
+        public EnemyDto Enemy { get; init; }
+            = null!;
+
+        public Guid CombatRoomId { get; init; }
+    }
+}
