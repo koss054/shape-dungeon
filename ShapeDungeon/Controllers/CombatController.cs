367e978c50d803ad06c0f38572b01928a4a8a0a7
koss054 <konstantin054@abv.bg>
2023-06-30 16:52:56 +0300

    Create initial Combat controller with debug view

---
+++ ShapeDungeon/Controllers/CombatController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShapeDungeon.DTOs.Enemies;
+using ShapeDungeon.DTOs.Players;
+using ShapeDungeon.Interfaces.Services;
+using ShapeDungeon.Interfaces.Services.Enemies;
+
+namespace ShapeDungeon.Controllers
+{
+    public class CombatController : Controller
+    {
+        private readonly ICombatService _combatService;
+
+        public CombatController(ICombatService combatService)
+        {
+            _combatService = combatService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Action()
+        {
+            var combatDto = await _combatService.InitializeCombat();
+            return View(combatDto);
+        }
+    }
+}
