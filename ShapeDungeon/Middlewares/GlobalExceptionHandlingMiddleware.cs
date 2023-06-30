1e456eb80532a7543766e2a9295e19d724243ec8
koss054 <konstantin054@abv.bg>
2023-06-30 12:40:00 +0300

    Create GlobalExceptionHandlingMiddleware.cs

---
+++ ShapeDungeon/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace ShapeDungeon.Middlewares
+{
+    public class GlobalExceptionHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger _logger;
+
+        public GlobalExceptionHandlingMiddleware(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+
+                context.Response.StatusCode =
+                    (int)HttpStatusCode.InternalServerError;
+
+                var problemDetails = new ProblemDetails()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server error",
+                    Title = "Server error",
+                    Detail = e.Message
+                };
+
+                string json = JsonSerializer.Serialize(problemDetails);
+
+                await context.Response.WriteAsync(json);
+                context.Response.ContentType = "application/json";
+            }
+        }
+    }
+}
