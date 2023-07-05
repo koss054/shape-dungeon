40839b009e93c14a62bba90e7b092be1f3aee332
koss054 <konstantin054@abv.bg>
2023-07-05 19:49:48 +0300

    Get player stats
    Added a response class, a new method in the PlayerGetService and an "endpoint" in the new PlayerResponseController.

---
+++ ShapeDungeon/Controllers/PlayerResponseController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShapeDungeon.Interfaces.Services.Players;
+
+namespace ShapeDungeon.Controllers
+{
+    public class PlayerResponseController : Controller
+    {
+        private readonly IPlayerGetService _playerGetService;
+
+        public PlayerResponseController(IPlayerGetService playerGetService)
+        {
+            _playerGetService = playerGetService;
+        }
+
+        public async Task<IActionResult> GetStats()
+        {
+            var playerStats = await _playerGetService.GetActivePlayerStats();
+            return Json(playerStats);
+        }
+    }
+}
---
+++ ShapeDungeon/Responses/Players/PlayerStatsResponse.cs
@@ -0,0 +1,19 @@
+﻿using ShapeDungeon.Entities;
+
+namespace ShapeDungeon.Responses.Players
+{
+    public class PlayerStatsResponse
+    {
+        public int Strength { get; init; }
+        public int Vigor { get; init; }
+        public int Agility { get; init; }
+
+        public static implicit operator PlayerStatsResponse(Player player)
+            => new()
+            {
+                Strength = player.Strength,
+                Vigor = player.Vigor,
+                Agility = player.Agility,
+            };
+    }
+}
