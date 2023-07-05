469c4412864bcccd597871b8fdfa0eb1495e34c5
koss054 <konstantin054@abv.bg>
2023-07-05 18:48:25 +0300

    Move combat script to separate file

---
+++ ShapeDungeon/wwwroot/js/combat/combat-action.js
@@ -0,0 +1,22 @@
+﻿const attackBtn = document.getElementById("attack-btn");
+const enemyHpEl = document.getElementById("enemy-hp");
+const playerStrengthEl = document.getElementById("player-strength");
+
+attackBtn.addEventListener("click", attackEnemy);
+
+function attackEnemy() {
+    fetch("/Combat/Test", {
+        method: "PATCH",
+        body: JSON.stringify({ hp: 54 }),
+        headers: { "Content-type": "application/json;" }
+    })
+        .then(response => response.json())
+        .then(() => updateEnemyHp())
+
+}
+
+function updateEnemyHp() {
+    fetch("/Combat/Test2")
+        .then(response => response.json())
+        .then(enemyInfo => enemyHpEl.innerText = enemyInfo.hp);
+}
