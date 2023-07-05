492a5ac963de9f8fd9f071ad1fc5e6f00ac42d23
koss054 <konstantin054@abv.bg>
2023-07-05 19:07:34 +0300

    Add functional enemy health bar

--- ShapeDungeon/wwwroot/js/combat/combat-action.js
+++ ShapeDungeon/wwwroot/js/combat/combat-action.js
@@ -1,13 +1,19 @@
-﻿const attackBtn = document.getElementById("attack-btn");
-const enemyHpEl = document.getElementById("enemy-hp");
+﻿import { updateEnemyHpBar } from "../combat/combat-hp-bars.js";
+
+const attackBtn = document.getElementById("attack-btn");
 const playerStrengthEl = document.getElementById("player-strength");
 
+const currentHpEnemyEl = document.getElementById("enemy-current-hp");
+const totalHpEnemyEl = document.getElementById("enemy-total-hp");
+
 attackBtn.addEventListener("click", attackEnemy);
+
+onCombatPageLoad();
 
 function attackEnemy() {
     fetch("/Combat/Test", {
         method: "PATCH",
-        body: JSON.stringify({ hp: 54 }),
+        body: JSON.stringify({ hp: 1 }),
         headers: { "Content-type": "application/json;" }
     })
         .then(response => response.json())
@@ -18,5 +24,13 @@ function attackEnemy() {
 function updateEnemyHp() {
     fetch("/Combat/Test2")
         .then(response => response.json())
-        .then(enemyInfo => enemyHpEl.innerText = enemyInfo.hp);
+        .then(enemyInfo => {
+            currentHpEnemyEl.innerText = enemyInfo.hp;
+            updateEnemyHpBar(enemyInfo.hp, totalHpEnemyEl.innerText);
+        });
+}
+
+function onCombatPageLoad() {
+    // Update hp bars.
+    updateEnemyHpBar(currentHpEnemyEl.innerText, totalHpEnemyEl.innerText);
 }
---
+++ ShapeDungeon/wwwroot/js/combat/combat-hp-bars.js
@@ -0,0 +1,6 @@
+﻿const enemyHpBarEl = document.getElementById("enemy-health-bar");
+
+export function updateEnemyHpBar(currentHp, totalHp) {
+    const barPercentage = (Number(currentHp) * 100) / Number(totalHp);
+    enemyHpBarEl.style.width = `${barPercentage}%`;
+}
