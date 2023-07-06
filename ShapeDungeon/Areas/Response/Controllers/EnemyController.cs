56a00dc925d8bf09c9c274fe59289fa82baca2f9
koss054 <konstantin054@abv.bg>
2023-07-06 16:54:53 +0300

    Create initial enemy response controller
    Includes EnemyStatResponse.

---
+++ ShapeDungeon/Areas/Response/Controllers/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShapeDungeon.Interfaces.Services.Enemies;
+using ShapeDungeon.Responses.Enemies;
+
+namespace ShapeDungeon.Areas.Response.Controllers
+{
+    [Route("Response/Enemy/")]
+    public class EnemyController : Controller
+    {
+        private readonly IEnemyGetService _enemyGetService;
+
+        public EnemyController(IEnemyGetService enemyGetService)
+        {
+            _enemyGetService = enemyGetService;
+        }
+
+        [Route("Stats")]
+        public async Task<IActionResult> GetCurrentEnemyStats(Guid enemyId)
+        {
+            var enemy = await _enemyGetService.GetById(enemyId);
+            EnemyStatResponse enemyStats = enemy;
+            return Json(enemyStats);
+        }
+    }
+}
