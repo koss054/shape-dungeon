7d16171ce944fd7b3cbccdb2a3e8e1c672641de6
koss054 <konstantin054@abv.bg>
2023-07-07 16:05:42 +0300

    Make defeated enemy appear less on screen
    Making it even more distinct compared to enemies that haven't been defeated.

--- ShapeDungeon/wwwroot/js/enemy/defeated-style.js
+++ ShapeDungeon/wwwroot/js/enemy/defeated-style.js
@@ -1,12 +1,12 @@
 ﻿const enemyShapeEl = document.getElementById("enemy-shape");
 const enemyLevelEl = document.getElementById("enemy-level");
 
-enemyLevelEl.style.color = "darkred";
+enemyLevelEl.style.color = "rgb(139 0 0 / 68%)";
 
 function defeatedEnemyTriangle() {
-    enemyShapeEl.style.borderLeftColor = "darkred";
+    enemyShapeEl.style.borderLeftColor = "#8b000040";
 }
 
 function defeatedEnemy() {
-    enemyShapeEl.style.backgroundColor = "darkred";
+    enemyShapeEl.style.backgroundColor = "#8b000040";
 }
