c2e3eff1c7293fec5726efbfb1f8b74dc938085d
koss054 <konstantin054@abv.bg>
2023-07-13 14:16:03 +0300

    Create IRoomCoordsRepositoryGet
    Trying to separate it a bit. Will ask if such separation is overkill.

---
+++ ShapeDungeon/Interfaces/Repositories/Rooms/IRoomCoordsRepositoryGet.cs
@@ -0,0 +1,12 @@
+﻿using ShapeDungeon.Entities;
+using ShapeDungeon.Specifications;
+
+namespace ShapeDungeon.Interfaces.Repositories.Rooms
+{
+    public interface IRoomCoordsRepositoryGet<T>
+        where T : class
+    {
+        Task<int> GetCoordXByAsync(ISpecification<Room> specification);
+        Task<int> GetCoordYByAsync(ISpecification<Room> specification);
+    }
+}
--- ShapeDungeon/Repos/Rooms/RoomGetRepository.cs
+++ ShapeDungeon/Repos/Rooms/RoomGetRepository.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ShapeDungeon.Entities;
 using ShapeDungeon.Interfaces.Repositories;
+using ShapeDungeon.Interfaces.Repositories.Rooms;
 using ShapeDungeon.Specifications;
 
 namespace ShapeDungeon.Repos.Rooms
 {
-    public class RoomGetRepository : RepositoryBase<Room>, IRepositoryGet<Room>
+    public class RoomGetRepository : RepositoryBase<Room>, 
+        IRepositoryGet<Room>, IRoomCoordsRepositoryGet<Room>
     {
         public RoomGetRepository(IDbContext context) : base(context)
         {
@@ -24,6 +26,30 @@ namespace ShapeDungeon.Repos.Rooms
 
             return roomToReturn ?? throw new ArgumentNullException(
                 nameof(roomToReturn), "No room matches provided specification.");
+        }
+
+        public async Task<int> GetCoordXByAsync(ISpecification<Room> specification)
+        {
+            var expression = specification.ToExpression();
+            var coordToReturn = await this.Context.Rooms
+                .AsQueryable()
+                .Where(expression)
+                .Select(x => x.CoordX)
+                .FirstOrDefaultAsync();
+
+            return coordToReturn;
+        }
+
+        public async Task<int> GetCoordYByAsync(ISpecification<Room> specification)
+        {
+            var expression = specification.ToExpression();
+            var coordToReturn = await this.Context.Rooms
+                .AsQueryable()
+                .Where(expression)
+                .Select(x => x.CoordY)
+                .FirstOrDefaultAsync();
+
+            return coordToReturn;
         }
     }
 }
