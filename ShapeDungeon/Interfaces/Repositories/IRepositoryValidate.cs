0419b6b1989fac592ef3e74b4691c7ae62c971d1
koss054 <konstantin054@abv.bg>
2023-07-13 14:36:29 +0300

    Create IRepositoryValidate.cs
    Not used but present just in case.

---
+++ ShapeDungeon/Interfaces/Repositories/IRepositoryValidate.cs
@@ -0,0 +1,10 @@
+﻿using ShapeDungeon.Specifications;
+
+namespace ShapeDungeon.Interfaces.Repositories
+{
+    public interface IRepositoryValidate<T>
+        where T : class
+    {
+        Task<bool> IsValidByAsync(ISpecification<T> specification);
+    }
+}
