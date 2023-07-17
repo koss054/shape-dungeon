73c0efa4ad976b6614933e4f5f2eec578b55899a
koss054 <konstantin054@abv.bg>
2023-07-17 23:55:44 +0300

    Create EnemyRoomDefeatedSpecification.cs

---
+++ ShapeDungeon/Specifications/EnemiesRooms/EnemyRoomDefeatedSpecification.cs
@@ -0,0 +1,18 @@
+﻿using ShapeDungeon.Entities;
+using System.Linq.Expressions;
+
+namespace ShapeDungeon.Specifications.EnemiesRooms
+{
+    public class EnemyRoomDefeatedSpecification : Specification<EnemyRoom>
+    {
+        public EnemyRoomDefeatedSpecification()
+        {
+        }
+
+        public override bool IsSatisfiedBy(EnemyRoom enemyRoom)
+            => enemyRoom.IsEnemyDefeated;
+
+        public override Expression<Func<EnemyRoom, bool>> ToExpression()
+            => enemyRoom => enemyRoom.IsEnemyDefeated;
+    }
+}
