ffb711a42f78da44497ae64f8e9fb7eba8ef6031
koss054 <konstantin054@abv.bg>
2023-07-17 23:01:31 +0300

    Create EnemyIdSpecification.cs

---
+++ ShapeDungeon/Specifications/Enemies/EnemyIdSpecification.cs
@@ -0,0 +1,21 @@
+﻿using ShapeDungeon.Entities;
+using System.Linq.Expressions;
+
+namespace ShapeDungeon.Specifications.Enemies
+{
+    public class EnemyIdSpecification : Specification<Enemy>
+    {
+        private readonly Guid _id;
+
+        public EnemyIdSpecification(Guid id)
+        {
+            _id = id;
+        }
+
+        public override bool IsSatisfiedBy(Enemy enemy)
+            => enemy.Id.Equals(_id);
+
+        public override Expression<Func<Enemy, bool>> ToExpression()
+            => enemy => enemy.Id.Equals(_id);
+    }
+}
