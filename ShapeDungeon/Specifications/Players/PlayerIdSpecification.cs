711841b3ba132d5af49b5623973000be4bbd3a2a
koss054 <konstantin054@abv.bg>
2023-07-19 17:13:00 +0300

    Add initial player specifications

---
+++ ShapeDungeon/Specifications/Players/PlayerIdSpecification.cs
@@ -0,0 +1,21 @@
+﻿using ShapeDungeon.Entities;
+using System.Linq.Expressions;
+
+namespace ShapeDungeon.Specifications.Players
+{
+    public class PlayerIdSpecification : Specification<Player>
+    {
+        private readonly Guid _id;
+
+        public PlayerIdSpecification(Guid id)
+        {
+            _id = id;
+        }
+
+        public override bool IsSatisfiedBy(Player player)
+            => player.Id.Equals(_id);
+
+        public override Expression<Func<Player, bool>> ToExpression()
+            => player => player.Id.Equals(_id);
+    }
+}
