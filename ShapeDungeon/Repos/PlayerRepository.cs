179ae5e48d0a25964d49fb6f2e26810afdc4d1e6
koss054 <konstantin054@abv.bg>
2023-07-19 16:50:50 +0300

    Create new player repository

---
+++ ShapeDungeon/Repos/PlayerRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShapeDungeon.Entities;
+using ShapeDungeon.Interfaces.Repositories;
+using ShapeDungeon.Specifications;
+
+namespace ShapeDungeon.Repos
+{
+    public class PlayerRepository : RepositoryBase<Player>, IPlayerRepository
+    {
+        public PlayerRepository(IDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Player>> GetMultipleByAsync(ISpecification<Player> specification)
+        {
+            var expression = specification.ToExpression();
+            var playersToReturn = await this.Context.Players
+                .AsQueryable()
+                .Where(expression)
+                .ToListAsync();
+
+            return playersToReturn;
+        }
+
+        public async Task<Player> GetFirstAsync(ISpecification<Player> specification)
+        {
+            var expression = specification.ToExpression();
+            var playerToReturn = await this.Context.Players
+                .AsQueryable()
+                .Where(expression)
+                .FirstOrDefaultAsync();
+
+            return playerToReturn ?? throw new ArgumentNullException(
+                nameof(playerToReturn), "No enemy matches provided specification.");
+        }
+
+        public async Task<bool> IsValidByAsync(ISpecification<Player> specification)
+        {
+            var expression = specification.ToExpression();
+            var boolToReturn = await this.Context.Players
+                .AsQueryable()
+                .Where(expression)
+                .AnyAsync();
+
+            return boolToReturn;
+        }
+
+        public void Update(Player player)
+        {
+            this.Context.Players.Update(player);
+        }
+
+        public async Task AddAsync(Player player)
+            => await this.Context.Players.AddAsync(player);
+    }
+}
